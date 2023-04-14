053c57497c5bb4b9d3475d9eda9514c4d75bffb5
swayam-learning <swayamswarupbarik25@gmail.com>
2023-04-14 13:36:13 +0530

    commented out a part of the program

---
+++ basics.cpp
@@ -0,0 +1,21 @@
+// #include<stdio.h>
+// int main()
+
+// {
+//     printf("geeksforgeeks");
+//     return 0;
+// }
+// variables are name that points to some memory
+// declaration of variable 
+//size of variable and name of the variable
+#include<stdio.h>
+int main()
+{
+    int var=3;// semicolon separates two statements
+    // this was declaration of a variable
+    // assigining and declaring a value is called initilisation
+    var=4;
+    // each variable must be defined once in one block of code
+    printf("%d",var);
+    return 0;
+}
